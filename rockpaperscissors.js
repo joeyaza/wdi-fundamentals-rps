7755bd31e109293af185325221bb1a71c0906a8a
Joseph Azadehdel <Joes@Josephs-MacBook-Air.local>
2015-09-19 12:42:29 +0100

    getComputerMove

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -33,3 +33,11 @@ var player = move || getInput();
     return player;
 };
 
+function getComputerMove(move) {
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+
+var computer = move || randomPlay();
+    return computer;
+}       
